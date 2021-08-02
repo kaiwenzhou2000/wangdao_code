d4886260267a0865550c13eb254c4367a2805996
meredith <547906080@qq.com>
2021-08-02 21:16:03 +0800

    完成5.2

---
+++ 第五章_树与二叉树/5_2_二叉树的存储结构/public.h
@@ -0,0 +1,8 @@
+//
+// Created by kaiven on 2021/8/2.
+//
+
+#ifndef WANGDAO_CODE_PUBLIC_H
+#define WANGDAO_CODE_PUBLIC_H
+
+#endif //WANGDAO_CODE_PUBLIC_H
---
+++ 第五章_树与二叉树/5_2_二叉树的存储结构/第五题/5_2_5.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kaiven on 2021/8/2.
+//
+
+#include "5_2_5.h"
